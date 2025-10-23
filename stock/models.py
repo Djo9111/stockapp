87c8d42d1c99ead34e92e189b36dfe5465437b50
aliouneriddle <aliouneriddle@gmail.com>
2025-10-23 15:40:45 +0000

    suppression du readme disponible ailleurs

--- stock/models.py
+++ stock/models.py
@@ -1,19 +1,50 @@
 from django.db import models
 
 
-class MethodeValorisation(models.TextChoices):
-    FIFO = "FIFO", "FIFO"
-    LIFO = "LIFO", "LIFO"
-    MOYENNE = "MOY", "Moyenne pondérée"
+# class MethodeValorisation(models.TextChoices):
+#     FIFO = "FIFO", "FIFO"
+#     LIFO = "LIFO", "LIFO"
+#     MOYENNE = "MOY", "Moyenne pondérée"
 
 
 class Produit(models.Model):
     nom = models.CharField(max_length=254)
-    methode_valorisation = models.CharField(
-        max_length=4,
-        choices=MethodeValorisation.choices,
-        default=MethodeValorisation.FIFO
-    )
+    stock = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return self.nom
+
+    @property
+    def stockPdt(self):
+        return self.stock
+
+
+class MvtStock(models.Model):
+    """Model definition for MvtStock."""
+
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    quantite = models.PositiveSmallIntegerField()
+    sens = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        """Meta definition for MvtStock."""
+
+        verbose_name = "MvtStock"
+        verbose_name_plural = "MvtStocks"
+
+    def __str__(self):
+        """Unicode representation of MvtStock."""
+        pass
+
+    def entrees(self):
+        pass
+
+    def sorties(self):
+        pass
+
+    def stockactuel(self):
+        pass
+
+    def stock_a_date(self, date_stock):
+        pass
